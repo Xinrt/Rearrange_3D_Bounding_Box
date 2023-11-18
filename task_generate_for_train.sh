6115e49d104aac202cd7228d9071d12e9d283d89
Xinran Tang <xt2191@login4.fast>
2023-11-19 01:31:48 +0400

    typo

---
+++ task_generate_for_train.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH -p nvidia
+#SBATCH --gres=gpu:2
+#SBATCH --job-name=train_generate_data
+#SBATCH --output=/scratch/xt2191/luyi/Rearrange_3D_Bounding_Box/sbatch_output.log
+#SBATCH --error=/scratch/xt2191/luyi/Rearrange_3D_Bounding_Box/sbatch_error.log
+#SBATCH --time=48:00:00
+#SBATCH --mem=64GB
+
+# Setting up Vulkan SDK environment
+cd /scratch/xt2191/luyi/vulkan/1.2.189.0
+source setup-env.sh
+
+vulkaninfo
+
+# Extending PYTHONPATH
+export PYTHONPATH=$PYTHONPATH:/scratch/xt2191/luyi/ai2thor-rearrangement-main
+export PYTHONPATH=$PYTHONPATH:/scratch/xt2191/luyi/Rearrange_3D_Bounding_Box
+
+conda init
+source activate thor-rearrange
+
+cd /scratch/xt2191/luyi/Rearrange_3D_Bounding_Box
+cd scripts
+
+echo "Start running"
+
+python generate_openness_dataset.py --logdir "/scratch/xt2191/luyi/Dataset"

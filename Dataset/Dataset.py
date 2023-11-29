cdeaa05ac4ee2ccf8a81d2caa534d84cfe2c7cdc
luyi404 <luyi4me@gmail.com>
2023-11-29 17:24:32 -0500

    Update dataset class and collate function

--- Dataset/Dataset.py
+++ Dataset/Dataset.py
@@ -14,15 +14,17 @@ def read_image(file_path: str, color_mode: int) -> torch.Tensor:
 
     if color_mode == cv2.IMREAD_COLOR:
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = torch.from_numpy(image.transpose(2, 0, 1))  # HWC to CHW for color images
+    elif color_mode == cv2.IMREAD_GRAYSCALE:
+        image = torch.from_numpy(image).unsqueeze(0)  # Add channel dimension for grayscale images
 
-    return torch.from_numpy(image.transpose(2, 0, 1))
+    return image
 
-
-class Dataset(torch.utils.data.Dataset):
-    ANNOTATION_FOLDER = 'annotation'
+class MyDataset(torch.utils.data.Dataset):
+    ANNOTATION_FOLDER = 'annotations'
     RGB_FOLDER = 'rgb'
     DEPTH_FOLDER = 'depth'
-    SEGMENTATION_FOLDER = 'seg'
+    SEGMENTATION_FOLDER = 'sem'
     PAN_FOLDER = 'pan'
 
     def __init__(self, base_path: str):
@@ -54,26 +56,21 @@ class Dataset(torch.utils.data.Dataset):
         ))
 
         length = len(annotation_files_without_extension)
-
-        # use this length to check if the other folders have the same length
-        assert len(os.listdir(self.rgb_path)) == length
-        assert len(os.listdir(self.depth_path)) == length
-        assert len(os.listdir(self.segmentation_path)) == length
-        assert len(os.listdir(self.pan_path)) == length
+        assert all(len(os.listdir(path)) == length for path in [self.rgb_path, self.depth_path, self.segmentation_path, self.pan_path]), "Inconsistent dataset sizes"
 
         self.data_names = annotation_files_without_extension
-
+        
     def __len__(self):
         return len(self.data_names)
 
     def __getitem__(self, index: int):
         file_name = self.data_names[index]
         paths = {
-            'annotation': os.path.join(self.base_path, 'annotation', file_name + '.json'),
-            'rgb': os.path.join(self.base_path, 'rgb', file_name + '.jpg'),
-            'depth': os.path.join(self.base_path, 'depth', file_name + '.jpg'),
-            'segmentation': os.path.join(self.base_path, 'seg', file_name + '.png'),
-            'pan': os.path.join(self.base_path, 'pan', file_name + '.jpg'),
+            'annotation': os.path.join(self.annotation_path, file_name + '.json'),
+            'rgb': os.path.join(self.rgb_path, file_name + '-rgb.png'),
+            'depth': os.path.join(self.depth_path, file_name + '-depth.png'),
+            'segmentation': os.path.join(self.segmentation_path, file_name + '-sem.png'),
+            'pan': os.path.join(self.pan_path, file_name + '-pan.png'),
         }
 
         try:
@@ -83,8 +80,31 @@ class Dataset(torch.utils.data.Dataset):
             raise FileNotFoundError(f"Annotation file not found for index {index}")
 
         rgb_image = read_image(paths['rgb'], cv2.IMREAD_COLOR)
-        depth_image = read_image(paths['depth'], cv2.IMREAD_GRAYSCALE).unsqueeze(0)
-        segmentation_image = read_image(paths['segmentation'], cv2.IMREAD_GRAYSCALE).unsqueeze(0)
+        depth_image = read_image(paths['depth'], cv2.IMREAD_GRAYSCALE)
+        segmentation_image = read_image(paths['segmentation'], cv2.IMREAD_GRAYSCALE)
         pan_image = read_image(paths['pan'], cv2.IMREAD_COLOR)
 
-        return rgb_image, depth_image, segmentation_image, pan_image, annotation
+        _data = {
+            'rgb': rgb_image,
+            'depth': depth_image,
+            'sem': segmentation_image,
+            'pan': pan_image,
+            'annotation': annotation
+        }
+        
+        return _data
+    
+def custom_collate_fn(batch):
+    rgb = torch.stack([item['rgb'] for item in batch])
+    depth = torch.stack([item['depth'] for item in batch])
+    sem = torch.stack([item['sem'] for item in batch])
+    pan = torch.stack([item['pan'] for item in batch])
+    annotation = [item['annotation'] for item in batch]  # Handle metadata separately
+    
+    return {
+        'rgb': rgb,
+        'depth': depth,
+        'sem': sem,
+        'pan': pan,
+        'annotation': annotation
+    }
---
+++ Dataset/dataset_test.py
@@ -0,0 +1,23 @@
+from Dataset import MyDataset, custom_collate_fn
+from torch.utils.data import DataLoader
+
+DATASET_PATH = '/vast/xt2191/dataset'
+
+if __name__ == "__main__":
+    # may take a long time since we have too many files
+    dataset = MyDataset(DATASET_PATH)
+
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=0, collate_fn=custom_collate_fn)
+
+    print("Length: ", len(dataset))
+
+    for batch in dataloader:
+        print(batch['rgb'])
+        print("annotation: ", batch['annotation'])
+
+        print("rgb.shape: ", batch['rgb'].shape)
+        print("depth.shape: ", batch['depth'].shape)
+        print("segmentation.shape: ", batch['sem'].shape)
+        print("pan.shape: ", batch['pan'].shape)
+        break
+        

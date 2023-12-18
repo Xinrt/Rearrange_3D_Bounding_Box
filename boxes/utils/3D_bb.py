ab1367d98b3e718e50f4e6b6119a23efde8fb5f7
Xinrt <xrt8363@gmail.com>
2023-12-17 19:17:44 -0500

    Finalize 3D bb

--- boxes/utils/3D_bb.py
+++ boxes/utils/3D_bb.py
@@ -1,5 +1,9 @@
 import numpy as np
 from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import scipy.ndimage
+
 
 import math
 from YOLO_detection import generate_2dboxes
@@ -8,6 +12,25 @@ from Dataset.Dataset import MyDataset, custom_collate_fn
 
 
 DATASET_PATH = '/vast/xt2191/dataset'
+
+
+def median_filter_depth_image(depth_image, kernel_size=5):
+    """
+    Apply a median filter to the depth image to reduce noise.
+    """
+    return scipy.ndimage.median_filter(depth_image, size=kernel_size)
+
+def extract_median_depth_from_box(depth_image, box):
+    """
+    Extract the median depth value from the specified bounding box.
+    """
+    xmin, ymin, xmax, ymax = box.astype(int)
+    depth_box = depth_image[ymin:ymax, xmin:xmax]
+    if depth_box.size == 0:
+        return None
+    return np.median(depth_box[depth_box > 0])  # Exclude zero depth values
+
+
 
 # Function to find the corresponding ids for given key names
 def find_indexes_for_keys(keys, dictionary):
@@ -112,6 +135,7 @@ def estimate_3d_bounding_boxes(bounding_boxes_2d, index_boxes, depth_image, came
         ])
         bounding_boxes_3d[idx] = bbox_3d
 
+
     return bounding_boxes_3d
 
 
@@ -160,7 +184,7 @@ if __name__ == "__main__":
     for batch in dataloader:
         annotations = batch['annotation']
         rgb_image = batch['rgb']
-        depth_image = batch['depth'] / 255.0
+        depth_image = batch['depth'] / 255.0 
 
         for annotation in annotations:
             print("annotation: ", annotation)
@@ -195,13 +219,32 @@ if __name__ == "__main__":
         rgb_numpy = rgb_image.squeeze(0).permute(1, 2, 0).cpu().numpy()
 
         boxes_2d, obj_names = object_detection(rgb_numpy)
+
+        # 在这里添加边界框绘制代码
+        fig, ax = plt.subplots(1)
+        ax.imshow(rgb_numpy)
+        for box in boxes_2d:
+            x, y, w, h = box[0], box[1], box[2] - box[0], box[3] - box[1]
+            rect = patches.Rectangle((x, y), w, h, linewidth=2, edgecolor='r', facecolor='none')
+            ax.add_patch(rect)
+
+        # 保存图像到文件
+        output_path = "/scratch/xt2191/Rearrange_3D_Bounding_Box/boxes/utilsdetected_objects.png"  # 您可以修改这个路径为您希望的保存位置
+        plt.savefig(output_path, bbox_inches='tight')
+        plt.close(fig)
+
+
         ids_for_keys = find_indexes_for_keys(obj_names, CLASS_TO_COLOR)
         indexes_only = list(ids_for_keys.values())
         # print(indexes_only)
 
+
+
         depth_numpy = depth_image.squeeze().cpu().numpy()
         bb_3d = estimate_3d_bounding_boxes(boxes_2d, indexes_only, depth_numpy, agent_coord, camera_pose)
         print("3D: \n", bb_3d)
+
+
 
         ious = []
         for idx in indexes_only:
@@ -221,4 +264,4 @@ if __name__ == "__main__":
         else:
             print("No IOU calculated")
 
-        # import pdb; pdb.set_trace()
+        import pdb; pdb.set_trace()
